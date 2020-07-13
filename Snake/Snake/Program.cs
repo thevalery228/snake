377b92a37c2a9c9de79b1a1455511a8c1d8744a0
Alexander <sanyo.ck@yandex.ru>
2020-07-13 11:22:14 +0300

    Добавили 2 конструктора.

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -10,8 +10,19 @@ namespace Snake
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, world");
+            Point p1 = new Point(1, 3, '*');
+            p1.Draw();
+
+            Point p2 = new Point(4, 5, '#');
+            p2.Draw();
+
             Console.ReadLine();
+        }
+
+        static void Draw(int x, int y, char sym)
+        {
+            Console.SetCursorPosition(x, y);
+            Console.Write(sym);
         }
     }
 }
