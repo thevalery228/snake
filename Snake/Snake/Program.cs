0d0e1d8734b7482af281aa755f06e7e3d0ee09a6
Alexander <sanyo.ck@yandex.ru>
2020-07-13 12:07:47 +0300

    Создали класс горизонтальной линии

--- Snake/Snake/Program.cs
+++ Snake/Snake/Program.cs
@@ -14,25 +14,8 @@ namespace Snake
             Point p2 = new Point(4, 5, '#');
             p2.Draw();
 
-            SampleCollection<int> numList = new SampleCollection<int>();
-            numList.Add(0);
-            numList.Add(1);
-            numList.Add(2);
-
-            int x = numList[0];
-            int y = numList[1];
-            int z = numList[2];
-
-            for (int i=0; i<numList.Count; i++)
-            {
-                Console.WriteLine(numList[i]);
-            }
-
-            numList.RemoveAt(0);
-
-            SampleCollection<Point> pList = new SampleCollection<Point>();
-            pList.Add(p1);
-            pList.Add(p2);
+            HorizontalLine line = new HorizontalLine(5, 10, 8, '+');
+            line.Draw();
 
             Console.ReadKey();
         }
